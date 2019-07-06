31da975923afc7111c75dcc051a84d12876abfdf
SwethaUnnam <Uswetha01@gmail.com>
2019-07-06 11:14:24 +0530

    corejava assignment practice

---
+++ CoreJava/AssementExercise/com/covalense/javaapp/temperature/HeavyTemperatureException.java
@@ -0,0 +1,17 @@
+package com.covalense.javaapp.temperature;
+
+public class HeavyTemperatureException extends Exception {
+
+	String str = " Server room temperature is high";
+
+	@Override
+	public String toString() {
+		return str;
+	}
+
+	@Override
+	public String getMessage() {
+		return str;
+	}
+
+}
