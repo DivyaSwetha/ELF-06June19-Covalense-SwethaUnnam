937588c66a65c0d553ba1a1cbb0b3262c2d82ae1
SwethaUnnam <Uswetha01@gmail.com>
2019-07-18 18:50:43 +0530

    spring mvc

---
+++ Spring/emp-springmvc/src/main/java/com/covalense/emp/util/HibernateUtil.java
@@ -0,0 +1,36 @@
+package com.covalense.emp.util;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.covalense.emp.dto.EmployeeInfoBean;
+import com.covalense.emp.dto.EmployeeOtherInfoBean;
+
+
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory ;
+	
+	private static SessionFactory buildSessionFactory() {
+		return new Configuration()
+				.configure()
+				.addAnnotatedClass(EmployeeInfoBean.class)
+				.addAnnotatedClass(EmployeeOtherInfoBean.class)
+				.buildSessionFactory();
+	}
+	private HibernateUtil() {
+		
+	}
+	 public static SessionFactory getSessionFactory() {
+		 if(sessionFactory==null) {
+			 sessionFactory=buildSessionFactory();
+		 }
+		 return sessionFactory;
+	 }
+	
+	 public static Session openSession() {
+		 return getSessionFactory().openSession();
+	 }
+}
